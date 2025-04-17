df3a70f54841668095b45e58d2f078084b5ce0d2
Leolyciia <leonie.hoekstra10@gmail.com>
2025-04-17 15:09:43 +0200

    blob

--- src/ball_tracker/include/ball_tracker.hpp
+++ src/ball_tracker/include/ball_tracker.hpp
@@ -1,3 +1,45 @@
+// #ifndef BALL_TRACKER_HPP_
+// #define BALL_TRACKER_HPP_
+
+// #include <rclcpp/rclcpp.hpp>
+// #include <sensor_msgs/msg/image.hpp>
+// #include <geometry_msgs/msg/point.hpp>
+// #include "cv_bridge/cv_bridge.h"
+// #include <opencv2/opencv.hpp> 
+
+// namespace ball_tracker
+// {
+
+// class BallTracker : public rclcpp::Node {
+// public:
+//   /**
+//    * @brief Construct a new ball tracker object
+//    */
+//   BallTracker();
+
+// private:
+//   /**
+//    * @brief Callback function for processing incoming image messages.
+//    * @param msg Shared pointer to the incoming sensor_msgs::msg::Image message.
+//    */
+//   void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
+
+//   // ROS 2 subscribers and publishers
+//   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+//   rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr ball_pub_;
+
+//   // Parameters for the HSV thresholding and filtering based on size
+//   int h_min_, h_max_;
+//   int s_min_, s_max_;
+//   int v_min_, v_max_;
+//   int size_min_px_, size_max_px_;
+// };
+
+// } // namespace ball_tracker
+
+// #endif // BALL_TRACKER_HPP_
+
+
 #ifndef BALL_TRACKER_HPP_
 #define BALL_TRACKER_HPP_
 
@@ -5,34 +47,28 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <geometry_msgs/msg/point.hpp>
 #include "cv_bridge/cv_bridge.h"
-#include <opencv2/opencv.hpp> 
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d.hpp>
 
 namespace ball_tracker
 {
 
 class BallTracker : public rclcpp::Node {
 public:
-  /**
-   * @brief Construct a new ball tracker object
-   */
   BallTracker();
 
 private:
-  /**
-   * @brief Callback function for processing incoming image messages.
-   * @param msg Shared pointer to the incoming sensor_msgs::msg::Image message.
-   */
   void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
 
-  // ROS 2 subscribers and publishers
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
   rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr ball_pub_;
 
-  // Parameters for the HSV thresholding and filtering based on size
   int h_min_, h_max_;
   int s_min_, s_max_;
   int v_min_, v_max_;
   int size_min_px_, size_max_px_;
+
+  cv::Ptr<cv::SimpleBlobDetector> blob_detector_;
 };
 
 } // namespace ball_tracker
